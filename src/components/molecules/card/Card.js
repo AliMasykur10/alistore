6a16b30bb5dc614ff78eae98ad550f6984a02d98
AliMasykur10 <alimasykur2001@gmail.com>
2023-07-03 06:46:47 +0700

    menambahkan hover pada card

--- src/components/molecules/card/Card.js
+++ src/components/molecules/card/Card.js
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function Card({ data }) {
   return (
-    <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col justify-between">
+    <div className="bg-white shadow-lg p-4 flex flex-col justify-between mb-3">
       <div className=" sm:h-80 grid place-items-center">
-        <img src={data.image} alt="#" className="w-44 object-cover mb-4" />
+        <img src={data.image} alt="#" className="w-44 object-cover mb-4 hover:scale-125 ease-in-out duration-200" />
       </div>
       <div className=" ">
-        <h3 className=" text-xl sm:text-lg font-semibold mb-2">{data.title}</h3>
+        <h3 className=" sm:text-lg font-semibold mb-2">{data.title}</h3>
         <p className="text-gray-700 mb-4">$15.00</p>
       </div>
       <div className=" flex justify-around">
-        <button className="bg-blue-500 hover:bg-blue-600 outline outline-1 font-semibold py-2 px-4">Buy Now</button>
-        <button className="bg-blue-500 hover:bg-blue-600 outline outline-1 font-semibold py-2 px-4">Add to Cart</button>
+        <button className="bg-blue-500 hover:bg-blue-600 outline outline-1 font-semibold py-2 px-4 hover:bg-fontColor hover:text-white ">Buy Now</button>
+        <button className="bg-blue-500 hover:bg-blue-600 outline outline-1 font-semibold py-2 px-4 hover:bg-fontColor hover:text-white ">Add to Cart</button>
       </div>
     </div>
   );
