9dfb9b3e28233d7fcb3246536bfcfe1367e5e75d
AliMasykur10 <alimasykur2001@gmail.com>
2023-07-03 06:52:34 +0700

    menambahkan durasi hover

--- src/components/molecules/card/Card.js
+++ src/components/molecules/card/Card.js
@@ -8,11 +8,11 @@ export default function Card({ data }) {
       </div>
       <div className=" ">
         <h3 className=" sm:text-lg font-semibold mb-2">{data.title}</h3>
-        <p className="text-gray-700 mb-4">$15.00</p>
+        <p className="text-gray-700 mb-4">${data.price}</p>
       </div>
       <div className=" flex justify-around">
-        <button className="bg-blue-500 hover:bg-blue-600 outline outline-1 font-semibold py-2 px-4 hover:bg-fontColor hover:text-white ">Buy Now</button>
-        <button className="bg-blue-500 hover:bg-blue-600 outline outline-1 font-semibold py-2 px-4 hover:bg-fontColor hover:text-white ">Add to Cart</button>
+        <button className="bg-blue-500 hover:bg-blue-600 outline outline-1 font-semibold py-2 px-4 hover:bg-fontColor hover:text-white transition-colors ">Buy Now</button>
+        <button className="bg-blue-500 hover:bg-blue-600 outline outline-1 font-semibold py-2 px-4 hover:bg-fontColor hover:text-white transition-colors ">Add to Cart</button>
       </div>
     </div>
   );
